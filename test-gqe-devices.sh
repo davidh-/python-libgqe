4d0d455e8327d7545d077467d5257981db343a91
davidh- <davidhooper4@berkeley.edu>
2023-03-05 00:02:50 -0800

    updated test script

--- test-gqe-devices.sh
+++ test-gqe-devices.sh
@@ -1,11 +1,18 @@
+# debugging usb hub adapater 2/21/23
 i=0
-while [ $i -le 100 ]
+while [ $i -le 100000 ]
 do
 	echo "Try: $i"
-	echo "CPM:"
+	echo -n "CPM: "
 	./gqe-cli /dev/ttyUSB1 --unit GMC500Plus --revision 'Re 2.42'  --get-cpm
-	echo "\nEMF:"
+	echo ""
+	echo -n "EMF: "
 	./gqe-cli /dev/ttyUSB0 --unit GQEMF390 --revision 'Re 3.70'  --get-emf
 	i=$(( $i + 1 ))
-	echo "\n"
+	echo ""
+	sleep 1
 done
+
+#from usb.core import find as finddev
+#dev = finddev(idVendor=0x1a40, idProduct=0x0101)
+#dev.reset()
